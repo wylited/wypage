5cc9be10da8dc5fa20288cc48c6e94e00a7141fc
wylited <wylited@gmail.com>
2023-03-21 21:53:26 +0800

    rebasing, initial commit

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,svelte}"],  theme: {
-  extend: {  fontFamily: {
-    'Montserrat': ["'Montserrat'", 'sans-serif'],
-    'Oswald': ["'Oswald'" ,'sans-serif'],
-  },},
-},
-plugins: [require("daisyui")],
-}
